d4518b696b02410d2707dc32bc7cb322aa291149
Harsh <harsh.here17@gmail.com>
2022-06-22 20:08:15 +0530

    restructure files

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Login from "../login/Login";
+
+function App() {
+  return (
+    // using BEM conventions
+    <div className="app">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
